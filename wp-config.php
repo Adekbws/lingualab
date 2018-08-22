c49f4d04a2c31810d91e431cf0316ec4b35a7eeb
Krystian W <krystian.w@seoheroes.pl>
2018-08-22 08:36:35 +0200

    db data

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'lingualab');
+define('DB_NAME', 'baza23124_lingualab');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'admin23124_lingualab');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '7Iyj4Qp1HX456814');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '23124.m.tld.pl');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
